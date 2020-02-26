fb2309b7bba89d1321c2470d2c4c7a05e2841455
Josh Tukman <josh.tukman@gmail.com>
2020-02-26 10:25:36 -0700

    Add ability to return item stock when empty

---
+++ test/vendor_test.rb
@@ -0,0 +1,89 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+require './lib/item'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+    assert_instance_of Item, @item1
+    assert_instance_of Item, @item2
+  end
+
+  def test_it_has_attributes
+    assert_equal "Tomato", @item2.name
+    assert_equal 0.50, @item2.price
+    assert_equal Hash.new, @vendor.inventory
+  end
+
+  def test_it_can_return_item_stock_when_empty
+    assert_equal 0, @vendor.check_stock(@item1)
+  end
+
+end
+
+
+
+
+
+
+# The Market will need to keep track of its Vendors and their Items. Each Vendor will be able to report its total inventory, stock items, and return the quantity of items. Any item not in stock should return `0` by default.
+#
+# Use TDD to create a `Vendor` class that responds to the following interaction pattern:
+#
+# ```ruby
+# pry(main)> require './lib/item'
+# #=> true
+#
+# pry(main)> require './lib/vendor'
+# #=> true
+#
+# pry(main)> item1 = Item.new({name: 'Peach', price: "$0.75"})
+# #=> #<Item:0x007f9c56740d48...>
+#
+# pry(main)> item2 = Item.new({name: 'Tomato', price: '$0.50'})
+# #=> #<Item:0x007f9c565c0ce8...>
+#
+# pry(main)> item2.name
+# #=> "Tomato"
+#
+# pry(main)> item2.price
+# #=> 0.50
+
+# pry(main)> vendor = Vendor.new("Rocky Mountain Fresh")
+# #=> #<Vendor:0x00007f85683152f0...>
+#
+# pry(main)> vendor.name
+# #=> "Rocky Mountain Fresh"
+#
+# pry(main)> vendor.inventory
+# #=> {}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 0
+#
+# pry(main)> vendor.stock(item1, 30)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 30}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 30
+#
+# pry(main)> vendor.stock(item1, 25)
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 55
+#
+# pry(main)> vendor.stock(item2, 12)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
+# ```
